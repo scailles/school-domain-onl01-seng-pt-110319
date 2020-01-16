bbeea4505893b5ec4c5c94efd9d9e811c033351a
Shaina C <scailles23@gmail.com>
2020-01-16 08:40:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,12 +15,11 @@ class School
   end
   
  def add_student(name, grade)
-if @roster[grade] != nil
-@roster[grade] << name
-else
-@roster[grade] = [name]
-end
-
+  if @roster[grade] != nil
+    @roster[grade] << name
+  else
+    @roster[grade] = [name]
+  end
 end
 
 
