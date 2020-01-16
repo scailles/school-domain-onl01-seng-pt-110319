5377143b4dd881f9a7118de0c492648ed4c3da27
Shaina C <scailles23@gmail.com>
2020-01-16 08:34:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  
+  
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+  
+  def name
+    @name
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade].empty?
+      << student
+    @roster[grade] << student
+  end
+  end
+    
+
+
+end
